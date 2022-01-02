f0626fdb2121de9881f6dbec6cb35af0448ffcc5
Jose Miguel Pedraza Mateo <jmpmateo3042013@gmail.com>
2022-01-01 21:36:43 -0500

    build: connected server to client main page

---
+++ components/FeaturedProduct.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="mainContent">
+    <div class="unified_widget pageBanner">
+      <h1>Books</h1>
+
+      <p>
+        <b
+          >Browse for more books <a href="#">leaders of notable people</a>, or
+          <a href="#">Fantasy Books</a> like Harry Potter or Game of Thrones
+          <a href="#">Best Fantasy books of the year so far</a></b
+        >
+      </p>
+    </div>
+    <div class="bx-root">
+      <div class="row">
+        <div class="col-sm-5 text-center">
+          <div class="bx-root-image">
+            <img
+              src="/img/featuredProduct.jpg"
+              alt="Featured product"
+              class="img-fluid"
+            />
+          </div>
+        </div>
+        <div class="col-sm-7 m-auto">
+          <div class="bx-root-dtls">
+            <div class="bx-root-heading">The Life and Universe</div>
+          </div>
+          <div class="bx-root-subtext">
+            Inside of the Mind of the legendary Scientist
+          </div>
+          <div class="bx-root-link">
+            <span>Order Now</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
